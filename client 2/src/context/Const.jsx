a850d620c71f32f66c4702dda7768bd4a20c4433
JackScofield <apuaimanlian@gmail.com>
2022-12-09 15:43:50 -0500

    ready to deploy

---
+++ client 2/src/context/Const.jsx
@@ -0,0 +1,5 @@
+// Our Brand
+export const BRAND = "Job Station";
+export const API_URL = "https://it4-back.onrender.com";
+export const CHAT_URL = "https://it4-chat-frug.onrender.com"
+// TODO use dotenv instead
--- client/src/context/Const.jsx
+++ client/src/context/Const.jsx
@@ -1,5 +1,5 @@
 // Our Brand
 export const BRAND = "Job Station";
-export const API_URL = "http://localhost:5001";
-export const CHAT_URL = "http://localhost:4000";
+export const API_URL = "https://main-server-wz28.onrender.com";
+export const CHAT_URL = "https://main-chat.onrender.com";
 // TODO use dotenv instead
