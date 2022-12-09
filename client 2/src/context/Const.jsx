bbf0091887d4e7d37d58ee79e81f535e9f38b851
Zeyuan Pu <apuaimanlian@gmail.com>
2022-12-09 15:45:40 -0500

    Delete client 2 directory

--- client 2/src/context/Const.jsx
+++
@@ -1,5 +0,0 @@
-// Our Brand
-export const BRAND = "Job Station";
-export const API_URL = "https://it4-back.onrender.com";
-export const CHAT_URL = "https://it4-chat-frug.onrender.com"
-// TODO use dotenv instead
