acbd50c5b9e39fb7f950340160fc9305ad4c7fd4
Zeyuan Pu <apuaimanlian@gmail.com>
2023-03-18 08:34:30 -0400

    Update Const.jsx

--- client/src/context/Const.jsx
+++ client/src/context/Const.jsx
@@ -1,5 +1,4 @@
 // Our Brand
 export const BRAND = "Job Station";
-export const API_URL = "http://44.204.125.56:5001";
-export const CHAT_URL = "http://44.204.125.56:4000";
-// TODO use dotenv instead
+export const API_URL = "http://34.135.224.215:5001";
+export const CHAT_URL = "http://34.135.224.215:4000";
